19d4c51719cd34b3e4671bcd2140510e47a00641
Nethaji <95306667+nethaji-ravi@users.noreply.github.com>
2026-03-13 02:21:52 +0530

    Update API URL to production endpoint

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,5 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
+// export const url = 'http://localhost:4000'
+export const url = 'https://food-delevery-6z6f.onrender.com/'
